3eaca6ae1457ada3e7314a75b691280936ac5d61
saurabhJagtap8807 <saurabhjagtap@golokait.com>
2024-03-27 13:26:37 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className="ml-10">
+        <h1 className="text-blue-500">Hello</h1>
+      </div>
+    </>
+  );
+}
+
+export default App
